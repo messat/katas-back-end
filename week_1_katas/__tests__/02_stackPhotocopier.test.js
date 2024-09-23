07c22151d3cfe92a2929eeac941ebadce85d3ae6
messat <148489126+messat@users.noreply.github.com>
2024-09-23 16:38:17 +0100

    This commit kata ensures nested objects and array is pure

--- week_1_katas/__tests__/02_stackPhotocopier.test.js
+++ week_1_katas/__tests__/02_stackPhotocopier.test.js
@@ -1,7 +1,70 @@
 const stackPhotocopier = require("../02_stackPhotocopier");
 
-describe("stackPhotocopier", () => {
-  it("", () => {
-    // first test here
+describe("stackPhotocopier - all blueprint properties are unchanged", () => {
+  it("Blueprint copy returns a new reference in memory for the array an objects nested within the data structure", () => {
+    const blueprints = [
+      {
+        isCopy: false,
+        title: "A guide to goat rearing",
+        mainText: "Feed them",
+        author: "Katherine",
+      }]
+      const copyStack = stackPhotocopier(blueprints)
+      expect(blueprints).not.toBe(copyStack)
+      expect(blueprints[0]).not.toBe(copyStack[0])
+  });
+  it("Blueprint inner content has not been changed whilst copying", () => {
+    const blueprints = [
+      {
+        isCopy: false,
+        title: "A guide to goat rearing",
+        mainText: "Feed them",
+        author: "Katherine",
+      }]
+      stackPhotocopier(blueprints)
+      expect(blueprints).toEqual([
+        {
+          isCopy: false,
+          title: "A guide to goat rearing",
+          mainText: "Feed them",
+          author: "Katherine",
+        }])
+  });
+  it("Once the blueprint has been copied, the isCopy property has changed to true for each nested object. Checking the distance nested objects having new reference in memory", () => {
+    const blueprints = [
+      {
+        isCopy: false,
+        title: "A guide to goat rearing",
+        mainText: "Feed them",
+        author: "Katherine",
+      }, {
+        isCopy: false,
+        title: "A lonely title",
+      },
+      {
+        isCopy: false,
+        title: "Hello world",
+        subtitle: "A dev story",
+      }]
+      const copyStack = stackPhotocopier(blueprints)
+      expect(copyStack).toEqual([
+        {
+          isCopy: true,
+          title: "A guide to goat rearing",
+          mainText: "Feed them",
+          author: "Katherine",
+        },
+        {
+          isCopy: true,
+          title: "A lonely title",
+        },
+        {
+          isCopy: true,
+          title: "Hello world",
+          subtitle: "A dev story",
+        },
+      ])
+      expect(blueprints[1]).not.toBe(copyStack[1])
+
   });
 });
